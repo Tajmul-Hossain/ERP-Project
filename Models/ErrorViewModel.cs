fab9577218334945b9983fe459ca4d615c344d8a
Tajmul Hossain <tajmulhossain.it@gmail.com>
2025-08-31 21:39:29 +0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ERP_Project11.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
